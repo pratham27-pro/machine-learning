cf14bf13d15cd1b2f3e80d3f0e701541d5c3ab6e
Pratham Jain <2muchappy420@gmail.com>
2025-08-24 00:34:50 +0530

    prediction pipeline done

--- End to End ML Project/src/utils.py
+++ End to End ML Project/src/utils.py
@@ -4,6 +4,7 @@ import dill
 
 import numpy as np
 import pandas as pd
+import pickle
 from sklearn.metrics import r2_score
 from src.exception import CustomException
 
@@ -20,12 +21,13 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            param=param
 
             model.fit(X_train, y_train)
 
@@ -41,5 +43,14 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
